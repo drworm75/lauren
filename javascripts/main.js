ac6a1f41c22b63987167fd64fdd53d914b4e0909
drworm75 <dwgames@gmail.com>
2017-04-09 23:34:05 -0500

    Project complete. No errors in grunt

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,6 @@
-alert('JS Ready');
+
+$("#reveal").on('click', function() {
+	$(this).parent('div').prev('div').find('.hidden').removeClass('hidden');
+	$(this).replaceWith("<p>Lauren teachs great lessons! She offers the most modestly priced help at a cost of $2.00, or a scone.</p>");
+	console.log($(this).parent('div'));
+});
